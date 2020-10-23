c10d316e82be6ba5f92fd4aefb20ca7cd2f43b5e
awinecki <winecki@gmail.com>
2020-10-23 12:58:46 +0200

    add error handling and logging; redirect to both stdout and file

--- test.sh
+++ test.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 SLEEP=${SLEEP:-10}
+out="results.txt"
 
 while true; do
-  echo "[$(date)] Checking net speeds.."
-  echo -e "[$(date)] Checking internet speed via speedtest.net CLI tools..\n" >> results.txt
-  echo "Running speedtest.net via CLI.."
-  speedtest --progress=no >> results.txt
-  echo -e "\n" >> results.txt
+  echo -e "[$(date)] Checking internet speed via speedtest.net CLI tools..\n" | tee -a $out
+  echo "Connected to WiFi network:" | tee -a $out
+  /Sy*/L*/Priv*/Apple8*/V*/C*/R*/airport -I | grep SSID | tee -a $out
+  echo "Running speedtest.net via CLI.." | tee -a $out
+  speedtest --progress=no 2>&1 | tee -a $out
+  echo -e "\n" | tee -a $out
   echo "Sleeping for $SLEEP seconds.."
   sleep $SLEEP
 done
